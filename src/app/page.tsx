d5f6b47b721fe67a9ba92992f52b13188a0dec5a
KhuiHihi <khoikhuat0807@gmail.com>
2024-06-21 14:51:54 +0700

    Penguin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-// "use client";
-// import { darkAtom, settingsClickedAtom } from "@/atoms";
-// import Background from "@/components/Background";
-// import Clock from "@/components/Clock";
-// import Footer from "@/components/Footer";
-// import Header from "@/components/Header";
-// import Settings from "@/components/Settings";
-// import clsx from "clsx";
-// import { useAtomValue } from "jotai";
-
-// export default function Home() {
-//   const toggleDark = useAtomValue(darkAtom);
-//   return (
-//     <div className="w-screen h-screen absolute">
-//       {/* {settingsClicked ? <Settings /> : <Clock />} */}
-//       {/* <div className="absolute opacity-[0.99] z-10 w-[350px] h-[450px] bg-white">
-//         <Settings />
-//       </div>
-//       <div className="absolute opacity-[0.99] z-[1]">
-//         <Clock />
-//       </div> */}
-//       {/* SET THE BACKGROUND TO WHITE OR THE CONTENT WILL SHOW THRU IT EVEN IF ITS A LOWER Z-INDEX. SAVE YOURSELF FROM 2 DAYS OF WASTED TIME. */}
-//       <Settings />
-//       <Clock />
-//     </div>
-//   );
-// }
-
 "use client";
 import { darkAtom, settingsAtom, settingsClickedAtom } from "@/atoms";
 import Background from "@/components/Background";
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,17 +5,18 @@ import { useAtomValue } from "jotai";
 function Background() {
   const toggleDark = useAtomValue(darkAtom);
   return (
-    <div className="z-[2] absolute top-[65%] left-[50%]">
+    <>
       {!toggleDark ? (
-        <div>
-          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/black-cat-idle.gif)] hover:bg-[url(/black-cat-sleep.gif)]"></div>
+        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen  top-[65%]">
+          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/black-cat-idle.gif)] hover:bg-[url(/black-cat-sleep.gif)]" />
+          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/penguin-idle.gif)] hover:bg-[url(/penguin-explode.gif)]" />
         </div>
       ) : (
-        <div>
+        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen  top-[65%]">
           <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/ghost-idle.gif)] hover:bg-[url(/ghost-sleep.gif)]"></div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
