a6004add165839fef80849fd0e68e5282ebdcdb1
KhuiHihi <khoikhuat0807@gmail.com>
2024-06-21 23:25:09 +0700

    Pixar

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,13 +7,14 @@ function Background() {
   return (
     <>
       {!toggleDark ? (
-        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen  top-[65%]">
+        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen top-[65%]">
           <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/black-cat-idle.gif)] hover:bg-[url(/black-cat-sleep.gif)]" />
           <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/penguin-idle.gif)] hover:bg-[url(/penguin-explode.gif)]" />
         </div>
       ) : (
-        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen  top-[65%]">
-          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/ghost-idle.gif)] hover:bg-[url(/ghost-sleep.gif)]"></div>
+        <div className="z-[5] flex flex-row items-center justify-center absolute w-screen top-[65%]">
+          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/ghost-idle.gif)] hover:bg-[url(/ghost-sleep.gif)]" />
+          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/pixar-idle.gif)] hover:bg-[url(/pixar-off.gif)]" />
         </div>
       )}
     </>
