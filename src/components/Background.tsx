0891f808713012bdef59c30ccfdf9e2ed0600851
KhuiHihi <khoikhuat0807@gmail.com>
2024-06-25 19:32:46 +0700

    feat: fire dude added

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -16,6 +16,7 @@ function Background() {
         <div className="z-[5] flex flex-row items-center justify-center absolute w-screen top-[65%]">
           <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/ghost-idle.gif)] hover:bg-[url(/ghost-sleep.gif)]" />
           <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/pixar-idle.gif)] hover:bg-[url(/pixar-off.gif)]" />
+          <div className="-z-10 scale-[0.2] w-[160px] h-[160px] bg-[url(/fire-idle.gif)] hover:bg-[url(/fire-burn-out.gif)]" />
         </div>
       )}
     </>
